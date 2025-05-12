8ca61d9b1b52a20260ffad8931162d59e302ab63
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-05-12 14:47:12 +0800

    Update whm.sh

--- whm.sh
+++ whm.sh
@@ -10,7 +10,7 @@ fi
 if [ -z $UUID ]; then
 uuid=$(cat /proc/sys/kernel/random/uuid)
 fi
-curl -s -o "/home/$username/domains/$domain/public_html/index.js" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/beta/app.js"
+curl -s -o "/home/$username/domains/$domain/public_html/app.js" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/beta/app.js"
 curl -s -o "/home/$username/domains/$domain/public_html/package.json" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/beta/package.json"
 sed -i "s/ccbfa010-f92b-49cf-8b4b-ae94b0fac112/$uuid/g" "/home/$username/domains/$domain/public_html/app.js"
 sed -i "s/9527.9528/$domain/g" "/home/$username/domains/$domain/public_html/app.js"
