4346edd439ed63227e95275384c02703d65f8cda
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-05-12 16:34:37 +0800

    Update whm.sh

--- whm.sh
+++ whm.sh
@@ -10,8 +10,8 @@ fi
 if [ -z $UUID ]; then
 uuid=$(cat /proc/sys/kernel/random/uuid)
 fi
-curl -s -o "/home/$username/domains/$domain/public_html/app.js" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/beta/app.js"
-curl -s -o "/home/$username/domains/$domain/public_html/package.json" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/beta/package.json"
+curl -s -o "/home/$username/domains/$domain/public_html/app.js" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/main/app.js"
+curl -s -o "/home/$username/domains/$domain/public_html/package.json" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/main/package.json"
 sed -i "s/('UUID', '')/('UUID', '$uuid')/g" "/home/$username/domains/$domain/public_html/app.js"
 sed -i "s/('DOMAIN', '')/('DOMAIN', '$domain')/g" "/home/$username/domains/$domain/public_html/app.js"
 sed -i "s/('PORT', '')/('PORT', '$vl_port')/g" "/home/$username/domains/$domain/public_html/app.js"
