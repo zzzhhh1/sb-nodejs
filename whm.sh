c4ba96bc14b7baa446e03f84a8447d774be3f949
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-05-12 15:56:13 +0800

    Update whm.sh

--- whm.sh
+++ whm.sh
@@ -9,7 +9,7 @@ vl_port=$(shuf -i 10000-65535 -n 1)
 fi
 curl -s -o "/home/$username/domains/$domain/public_html/app.js" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/beta/app.js"
 curl -s -o "/home/$username/domains/$domain/public_html/package.json" "https://raw.githubusercontent.com/yonggekkk/vless-nodejs/beta/package.json"
-sed -i "s/ccbfa010-f92b-49cf-8b4b-ae94b0fac112/$uuid/g" "/home/$username/domains/$domain/public_html/app.js"
-sed -i "s/9527.9528/$domain/g" "/home/$username/domains/$domain/public_html/app.js"
-sed -i "s/54321;/$vl_port;/g" "/home/$username/domains/$domain/public_html/app.js"
+sed -i "s/('UUID', '')/('UUID', '$uuid')/g" "/home/$username/domains/$domain/public_html/app.js"
+sed -i "s/('DOMAIN', '')/('DOMAIN', '$domain')/g" "/home/$username/domains/$domain/public_html/app.js"
+sed -i "s/('PORT', '')/('PORT', '$vl_port')/g" "/home/$username/domains/$domain/public_html/app.js"
 echo "安装结束"
